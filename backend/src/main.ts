f696a9cdff73b8e0a306af9edaa0a2ba677d4826
juhanse <julien.hanse12@gmail.com>
2026-03-25 14:45:16 +0100

    add: enable cors for local frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule);
+	app.enableCors({
+		origin: 'http://localhost:3001',
+	});
+	await app.listen(process.env.PORT ?? 3000);
+	app.useLogger(['log', 'error', 'warn', 'debug', 'verbose']);
 }
+
 bootstrap();
