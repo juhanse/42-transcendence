485abefcbd0938841e0fccf8622c3e5d1bf5231e
juhanse <julien.hanse12@gmail.com>
2026-03-25 01:51:23 +0100

    add: current-user decorator

---
+++ backend/src/common/decorators/current-user.decorator.ts
@@ -0,0 +1,14 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export type JwtUser = {
+	sub: number;
+	email: string;
+	role: string;
+};
+
+export const CurrentUser = createParamDecorator(
+	(data: unknown, ctx: ExecutionContext) => {
+		const req = ctx.switchToHttp().getRequest();
+		return req.user;
+	},
+);
