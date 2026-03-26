66c6a7e5ef81cc4395fd9eabb471293e5c4daf46
juhanse <julien.hanse12@gmail.com>
2026-03-26 23:33:24 +0100

    fix: import game module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { GamesModule } from './games/games.module';
 
 @Module({
   imports: [
@@ -22,7 +23,8 @@ import { UsersModule } from './users/users.module';
       }),
     }),
     AuthModule,
-    UsersModule
+    UsersModule,
+    GamesModule
   ],
   controllers: [],
   providers: [],
