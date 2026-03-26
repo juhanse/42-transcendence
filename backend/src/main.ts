ff99c3a32a6d3c9d88283c8185d50e2f0177fda1
juhanse <julien.hanse12@gmail.com>
2026-03-26 23:51:13 +0100

    fix: lint error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	app.enableCors({
-		origin: 'http://localhost:3001',
-	});
-	await app.listen(process.env.PORT ?? 3000).catch(err => console.error(err));
-	app.useLogger(['log', 'error', 'warn', 'debug', 'verbose']);
+  const app = await NestFactory.create(AppModule);
+
+  app.useLogger(['log', 'error', 'warn', 'debug', 'verbose']);
+  app.enableCors({
+    origin: 'http://localhost:3001',
+  });
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 
-bootstrap();
+void bootstrap();
